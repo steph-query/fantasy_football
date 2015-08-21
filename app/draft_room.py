f43351c59477e54876aea20787a4b150ea169ce8
Nick James <nickjljames@gmail.com>
2015-08-20 20:57:44 -0700

    front end work

--- app/draft_room.py
+++ app/draft_room.py
@@ -13,22 +13,22 @@ def draft():
   teams = db.session.query(RosterSpot.roster_position, Player.name).filter(RosterSpot.player_id == Player.id).filter(RosterSpot.team_id == 0)
 
   players = Player.query.filter_by(available=True).order_by(Player.points.desc())
-
+  pick = DraftPick.query.filter_by(player_id = '').first()
   if request.method == "POST":
     print('drafter')
     if draft_player_form.validate_on_submit():
       print("player selected")
-      pick = DraftPick.query.filter_by(player_id = '').first()
+
       player = Player.query.filter_by(name=draft_player_form.name.data).first()
       
       player.available = False
       pick.player_id = player.id 
       roster_spots = RosterSpot.query.filter_by(team_id=pick.team_id)
-      open_spots = roster_spots.filter(RosterSpot.roster_position.like('%{}%'.format(player.position))).filter(RosterSpot.player_id == '')
+      open_spots = roster_spots.filter(RosterSpot.roster_position.like('%{}%'.format(player.position))).filter(RosterSpot.player_id == '').all()
+      bench = roster_spots.filter(RosterSpot.roster_position.like('%B%').filter(RosterSpot.player_id == ''))
       if len(open_spots) >= 1:
         open_spots[0].player_id = player.id
       else:
-        bench = roster_spots.filter(RosterSpot.roster_position.like('%B%').filter(RosterSpot.player_id == ''))
         if len(bench.all()) >= 1:
           bench[0].player_id = player.id
         else:
@@ -36,7 +36,7 @@ def draft():
       db.session.commit()
       
 
-  return render_template("draft_player.html", roster=teams.all(), draft_player_form=draft_player_form, players=players.all(), error=error)
+  return render_template("draft_player.html", teams=teams.all(), draft_player_form=draft_player_form, players=players.all(), pick=pick, error=error)
 
 
 
